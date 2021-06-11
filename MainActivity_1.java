7d5d768d330fec417d7c02236833538800ef9757
cheessus <85635960+cheessus@users.noreply.github.com>
2021-06-11 22:08:18 +0800

    lesson 4

--- MainActivity_1.java
+++ MainActivity_1.java
@@ -1,49 +1,41 @@
-package com.example.assignment3;
+package com.example.radiobutton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.RadioButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    private Button submit;
-    public String FullName,Phone,Address;
-    private EditText fName,phone,address;
-    private TextView fullname;
+    private RadioButton rbAndroid,rbWeb,rbJava;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        rbAndroid = (RadioButton) findViewById(R.id.rbAndroid);
+        rbWeb = (RadioButton) findViewById(R.id.rbWeb);
+        rbJava = (RadioButton) findViewById(R.id.rbJava);
 
-
-        submit = (Button) findViewById(R.id.submitBtn);
-        fName = (EditText) findViewById(R.id.eFname);
-        phone = (EditText) findViewById(R.id.ePhone);
-        address = (EditText) findViewById(R.id.eAddress);
-        submit.setOnClickListener(new View.OnClickListener() {
+        rbAndroid.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                showInfo();
+                Toast.makeText(MainActivity.this, "Android", Toast.LENGTH_SHORT).show();
             }
         });
-
-    }
-
-    public void showInfo(){
-        FullName = fName.getText().toString();
-        Phone = phone.getText().toString();
-        Address = address.getText().toString();
-
-        Intent intent = new Intent(this,Info.class);
-        intent.putExtra("ner",FullName);
-        intent.putExtra("utas",Phone);
-        intent.putExtra("hayg",Address);
-        startActivity(intent);
+        rbWeb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Web", Toast.LENGTH_SHORT).show();
+            }
+        });
+        rbJava.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Java", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
